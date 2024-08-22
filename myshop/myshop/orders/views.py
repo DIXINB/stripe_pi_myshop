6ba07196db3c044447b538505637877ceed0a66d
DIXINB <astkv21@mail.ru>
2024-08-22 22:18:01 +0300

    add inside folders pycache

--- myshop/myshop/orders/views.py
+++ myshop/myshop/orders/views.py
@@ -29,8 +29,7 @@ def order_create(request):
             # set the order in the session
             request.session['order_id'] = order.id
             # redirect for payment
-#            return redirect(reverse('payment:process'))
-            return redirect(reverse('payment:create-payment-intent'))
+            return redirect(reverse('payment:create-payment-intent'))   
     else:
         form = OrderCreateForm()
     return render(request,
